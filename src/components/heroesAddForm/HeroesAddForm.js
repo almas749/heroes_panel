48e2b897668f14939a4aecd547ec0535752b33cd
almas <shegetnpanda@gmail.com>
2023-04-28 12:19:32 +0600

    first commit

---
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -0,0 +1,112 @@
+import {useHttp} from '../../hooks/http.hook';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import store from '../../store';
+
+import { selectAll } from '../heroesFilters/filtersSlice';
+import { heroCreating } from '../heroesList/heroesSlice';
+import { useState } from 'react';
+// Задача для этого компонента:
+// Реализовать создание нового героя с введенными данными. Он должен попадать
+// в общее состояние и отображаться в списке + фильтроваться
+// Уникальный идентификатор персонажа можно сгенерировать через uiid
+// Усложненная задача:
+// Персонаж создается и в файле json при помощи метода POST
+// Дополнительно:
+// Элементы <option></option> желательно сформировать на базе
+// данных из фильтров
+
+const HeroesAddForm = () => {
+    const [heroName, setHeroName] = useState('');
+    const [heroDescr, setHeroDescr] = useState('');
+    const [heroElement, setHeroElement] = useState('');
+
+    const {filtersLoadingStatus} = useSelector(state => state.filters);
+    const filters = selectAll(store.getState());
+    const dispatch = useDispatch();
+    const {request} = useHttp();
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        const hero = {
+            id: uuidv4(),
+            name: heroName,
+            description: heroDescr,
+            element: heroElement
+        }
+
+        request("http://localhost:3001/heroes", "POST", JSON.stringify(hero))
+            .then(data => console.log(data, 'Created'))
+            .then(dispatch(heroCreating(hero)))
+            .catch(err => console.log(err));
+        
+        setHeroName('');
+        setHeroDescr('');
+        setHeroElement('');
+    }
+
+    const renderFilters = (filters, status) => {
+        if (status === "loading") {
+            return <option>Загрузка элементов</option>
+        } else if (status === "error") {
+            return <option>Ошибка загрузки</option>
+        }
+        
+        if (filters && filters.length > 0 ) {
+            return filters.map(({name, label}) => {
+                if (name === 'all')  return;
+
+                return <option key={name} value={name}>{label}</option>
+            })
+        }
+    }
+
+    return (
+        <form className="border p-4 shadow-lg rounded" onSubmit={onSubmitHandler}>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
+                <input 
+                    required
+                    type="text" 
+                    name="name" 
+                    className="form-control" 
+                    id="name" 
+                    value={heroName}
+                    placeholder="Как меня зовут?"
+                    onChange={(e) => setHeroName(e.target.value)}/>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="text" className="form-label fs-4">Описание</label>
+                <textarea
+                    required
+                    name="text" 
+                    className="form-control" 
+                    id="text" 
+                    value={heroDescr}
+                    placeholder="Что я умею?"
+                    style={{"height": '130px'}}
+                    onChange={(e) => setHeroDescr(e.target.value)}/>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
+                <select 
+                    required
+                    className="form-select" 
+                    id="element" 
+                    name="element"
+                    value={heroElement}
+                    onChange={(e) => setHeroElement(e.target.value)}>
+                    <option value="">Я владею элементом...</option>
+                    {renderFilters(filters, filtersLoadingStatus)}
+                </select>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Создать</button>
+        </form>
+    )
+}
+
+export default HeroesAddForm;
